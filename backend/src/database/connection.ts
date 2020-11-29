707dec635feb58400f167e61d8d6fb95425cd166
unknown <diebraga0055@gmail.com>
2020-11-29 14:00:31 +0000

    added routes + database connection

---
+++ backend/src/database/connection.ts
@@ -0,0 +1,12 @@
+import knex from 'knex';
+import path from 'path';
+// file deal with database connection
+
+const connection = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: path.resolve(__dirname, 'database.sqlite')
+  },
+})
+
+export default connection;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  console.log('list')
-
-  res.json([
-    'diego',
-    "sarah"
-  ])
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
